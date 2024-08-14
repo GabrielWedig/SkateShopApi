6175e54fccccf889341c718eb7ba8dcfbd81530d
Gabriel Garcia Wedig <gabriel.wedig@cwi.com.br>
2024-08-14 15:04:01 -0300

    WiP

---
+++ SkateShop.Api/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace SkateShop.Api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddCorsExtension(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins(new string[]
+                    {
+                        "http://localhost:5173"
+                    })
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                });
+            });
+        }
+    }
+}
